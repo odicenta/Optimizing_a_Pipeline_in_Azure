960dfeb63a43d8f885b5b434986f66136dcfa6d9
Oscar Dicenta <odicenta@gmail.com>
2021-04-17 22:44:26 +0000

    Saved work
    Hyoerparameter ready and model running. AutoML pending

--- train.py
+++ train.py
@@ -13,14 +13,15 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 # TODO: Create TabularDataset using TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+DATA_URL = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
+ds = TabularDatasetFactory.from_delimited_files(DATA_URL)
 
 x, y = clean_data(ds)
 
-# TODO: Split data into train and test sets.
+#  Split data into train and test sets.
 
-### YOUR CODE HERE ###a
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
 
 run = Run.get_context()
 
